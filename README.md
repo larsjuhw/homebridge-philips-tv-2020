b431f0513519aa9877d140b8761e2351dd519793
Lars Wolter <larswolter@me.com>
2021-01-20 16:55:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,149 @@
+# Homebridge-philips-tv-2020
+
+A Homebridge plugin to control your Philips TV. Made for the 43PUS7805 2020 model, but it should work for most non-Android TV version 6 API TVs.
+
+Uses API version 6 and port 1925.
+
+## Features
+
+* Turn your TV on and off using HomeKit
+* Basic TV remote controls on your device
+* Change volume of TV on your device
+* Change ambilight style using a source selector in Home
+
+![styles-preview](https://user-images.githubusercontent.com/39745476/105191104-00d1a400-5b37-11eb-9975-24625c92f669.png)
+![controls-preview](https://user-images.githubusercontent.com/39745476/105191429-5908a600-5b37-11eb-8eb9-9b8ce4079aff.png)
+
+## Remote
+
+To use the remote functionality, add the "Apple TV Remote" button to your control centre and open it. Your TV should be available in the list. The cursor and back buttons work as expected, but the Play-Pause button is currently configured to act as the ambilight button on your remote, and the Info button simulates the Home button.
+
+While you are in the remote menu, you can use the volume buttons of your device to adjust the volume of your TV (or the speaker connected to it with HDMI ARC).
+
+## Installation
+
+### Adding the accessory to HomeKit
+
+HomeKit currently only allows one bridge to add one Television accessory. Therefore,this plugin is configured as a separate accessory and you need to manually add it to HomeKit.
+1. Open the Home <img src='https://user-images.githubusercontent.com/3979615/78010622-4ea1d380-738e-11ea-8a17-e6a465eeec35.png' height='16.42px'> app on your device.
+2. Tap the Home tab, then tap <img src='https://user-images.githubusercontent.com/3979615/78010869-9aed1380-738e-11ea-9644-9f46b3633026.png' height='16.42px'>.
+3. Tap **Add Accessory**, and select **I Don't Have a Code or Cannot Scan**.
+4. Enter the Homebridge PIN, this can be found under the QR code in Homebridge UI or your Homebridge logs.
+
+### Config
+
+#### Required parameters
+
+* name - The name of your TV in HomeKit
+* ip - The IP address of your TV
+* mac_address - The MAC Address of your TV. Used to turn on the TV with WOL.
+
+#### Optional parameters
+* ambilight_styles - List of Ambilight style objects. See [Ambilight styles](#ambilight-styles) for more information.
+
+#### Example config
+
+```json
+{
+    "platform": "HomebridgePhilipsTV",
+    "name": "...",
+    "ip": "...",
+    "mac_address": "...",
+    "ambilight_styles": [
+        {
+            "name": "...",
+            "value": "...",
+            "type": "..."
+        }
+    ]
+}
+```
+
+See [Ambilight styles](#ambilight-styles) for an example array of styles.
+
+### Ambilight styles
+
+Adding the ambilight_styles parameter to your config will allow you to change between ambilight styles using a source selector in the Home app.
+
+![styles-preview](https://user-images.githubusercontent.com/39745476/105191104-00d1a400-5b37-11eb-9975-24625c92f669.png)
+
+Example ambilight object:
+```json
+{
+    "name": "Standard",
+    "value": "STANDARD",
+    "type": "FOLLOW_VIDEO"
+}
+```
+
+You can find the `value` and `type` parameter of the current ambilight style with a GET request to `tvip:1925/6/ambilight/currentconfiguration`. The returned `menuSetting` is the `value` in the ambilight object, and the returned `styleName` is the `type` parameter. Some styles do not work when using their menuSetting in the POST request to change style, in that case you can add the optional `str` parameter to the ambilight object and use the `stringValue` of the style.
+
+To (somewhat) find out which menuSettings your TV supports, you can `GET tvip:1925/6/ambilight/supportedstyles` and it will return a list of the three styleNames with their possible menuSetting values. However, this list will most likely contain many duplicates and styles that are not actually supported.
+
+#### Example styles
+
+```json
+[
+    {
+        "name": "Standard",
+        "value": "STANDARD",
+        "type": "FOLLOW_VIDEO"
+    },
+    {
+        "name": "Natural",
+        "value": "NATURAL",
+        "type": "FOLLOW_VIDEO"
+    },
+    {
+        "name": "Sports",
+        "value": "SPORTS",
+        "type": "FOLLOW_VIDEO"
+    },
+    {
+        "name": "Vivid",
+        "value": "VIVID",
+        "type": "FOLLOW_VIDEO"
+    },
+    {
+        "name": "Game",
+        "value": "GAME",
+        "type": "FOLLOW_VIDEO"
+    },
+    {
+        "name": "Lumina",
+        "value": "ENERGY_ADAPTIVE_BRIGHTNESS",
+        "type": "FOLLOW_AUDIO"
+    },
+    {
+        "name": "Retro",
+        "value": "VU_METER",
+        "type": "FOLLOW_AUDIO",
+        "str": "Retro"
+    },
+    {
+        "name": "Rhythm",
+        "value": "RANDOM_PIXEL_FLASH",
+        "type": "FOLLOW_AUDIO"
+    },
+    {
+        "name": "Hot Lava",
+        "value": "HOT_LAVA",
+        "type": "FOLLOW_COLOR"
+    },
+    {
+        "name": "Deep Water",
+        "value": "DEEP_WATER",
+        "type": "FOLLOW_COLOR"
+    },
+    {
+        "name": "Fresh Nature",
+        "value": "FRESH_NATURE",
+        "type": "FOLLOW_COLOR"
+    },
+    {
+        "name": "Warm White",
+        "value": "ISF",
+        "type": "FOLLOW_COLOR"
+    }
+]
+```
---
+++ index.js
@@ -0,0 +1,314 @@
+var request = require("request");
+var wol = require("wake_on_lan");
+
+const PLUGIN_NAME = 'homebridge-philips-tv-2020';
+const PLATFORM_NAME = 'HomebridgePhilipsTV';
+
+module.exports = (api) => {
+    api.registerPlatform(PLATFORM_NAME, PhilipsTelevisionPlugin);
+}
+
+class PhilipsTelevisionPlugin {
+    constructor(log, config, api) {
+        this.log = log;
+        this.config = config;
+        this.api = api;
+
+        var that = this;
+
+        this.Service = api.hap.Service;
+        this.Characteristic = api.hap.Characteristic;
+
+        // get the name
+        const tvName = this.config.name;
+        const tvIP = this.config.ip;
+        const mac_address = this.config.mac_address;
+        const baseURL = "http://" + tvIP + ":1925/6/";
+        const ambilightStyles = this.config.ambilight_styles || [];
+
+        this.log.info("Found " + ambilightStyles.length + " ambilight styles");
+
+        // Generate a UUID
+        const uuid = this.api.hap.uuid.generate('homebridge:my-tv-plugin' + tvName);
+
+        // Create platform accessory
+        this.tvAccessory = new api.platformAccessory(tvName, uuid);
+        this.tvAccessory.category = this.api.hap.Categories.TELEVISION;
+
+        // Add TV service to platform
+        const tvService = this.tvAccessory.addService(this.Service.Television);
+        tvService.setCharacteristic(this.Characteristic.ConfiguredName, tvName);
+        tvService.setCharacteristic(this.Characteristic.SleepDiscoveryMode, this.Characteristic.SleepDiscoveryMode.NOT_DISCOVERABLE);
+
+        // Add handlers for on/off events
+        tvService.getCharacteristic(this.Characteristic.Active)
+            .on('set', (newValue, callback) => {
+                that.log.info('set Active => setNewValue: ' + newValue);
+
+                if (newValue == 1) {
+                    // Turn on TV using WOL
+                    var wol_wake = function () {
+                        wol.wake(mac_address, function (error) {
+                            if (error) {
+                                that.log("WOL failed: %s", error);
+                            }
+                        })
+                    }
+
+                    wol_wake();
+                } else {
+                    // Turn off TV using Standby key POST request
+                    this.remoteButton("Standby");
+                }
+
+                tvService.updateCharacteristic(this.Characteristic.Active, 1);
+                callback(null);
+            })
+            .on('get', (callback) => {
+                // GET request to /6/powerstate to find current state
+                // if no response, .get() returns false
+                var state = 0;
+                that.get("powerstate", (result) => {
+
+                    if (result == false) {
+                        state = 0;
+                    } else {
+                        var response = JSON.parse(result);
+                        state = response.powerstate == "On" ? 1 : 0;
+                    }
+
+                    that.log("Got powerstate: " + state);
+
+                    callback(null, state);
+                });
+            });
+
+        // Set default source (ambilight style) identifier to 1
+        tvService.setCharacteristic(this.Characteristic.ActiveIdentifier, 1);
+
+        // Handle source (ambilight style) changes
+        tvService.getCharacteristic(this.Characteristic.ActiveIdentifier)
+            .on('set', (newValue, callback) => {
+                // The identifiers start at 1 while the array with styles starts at index 0,
+                // so subtract 1 from the new value.
+                const i = newValue - 1;
+
+                this.log.info('set Active Identifier => setNewValue: ' + newValue);
+                this.log.info("set ambilight configuration to " + ambilightStyles[i].name);
+
+                this.setAmbilight(ambilightStyles[i]);
+
+                callback(null);
+            });
+
+        // Handle TV remote inputs
+        tvService.getCharacteristic(this.Characteristic.RemoteKey)
+            .on('set', (newValue, callback) => {
+                switch (newValue) {
+                    case this.Characteristic.RemoteKey.REWIND: {
+                        this.log.info('set Remote Key Pressed: REWIND');
+                        this.remoteButton("Rewind");
+                        break;
+                    }
+                    case this.Characteristic.RemoteKey.FAST_FORWARD: {
+                        this.log.info('set Remote Key Pressed: FAST_FORWARD');
+                        this.remoteButton("FastForward");
+                        break;
+                    }
+                    case this.Characteristic.RemoteKey.NEXT_TRACK: {
+                        this.log.info('set Remote Key Pressed: NEXT_TRACK');
+                        this.remoteButton("Next");
+                        break;
+                    }
+                    case this.Characteristic.RemoteKey.PREVIOUS_TRACK: {
+                        this.log.info('set Remote Key Pressed: PREVIOUS_TRACK');
+                        this.remoteButton("Previous");
+                        break;
+                    }
+                    case this.Characteristic.RemoteKey.ARROW_UP: {
+                        this.log.info('set Remote Key Pressed: ARROW_UP');
+                        this.remoteButton("CursorUp");
+                        break;
+                    }
+                    case this.Characteristic.RemoteKey.ARROW_DOWN: {
+                        this.log.info('set Remote Key Pressed: ARROW_DOWN');
+                        this.remoteButton("CursorDown");
+                        break;
+                    }
+                    case this.Characteristic.RemoteKey.ARROW_LEFT: {
+                        this.log.info('set Remote Key Pressed: ARROW_LEFT');
+                        this.remoteButton("CursorLeft");
+                        break;
+                    }
+                    case this.Characteristic.RemoteKey.ARROW_RIGHT: {
+                        this.log.info('set Remote Key Pressed: ARROW_RIGHT');
+                        this.remoteButton("CursorRight");
+                        break;
+                    }
+                    case this.Characteristic.RemoteKey.SELECT: {
+                        this.log.info('set Remote Key Pressed: SELECT');
+                        this.remoteButton("Confirm");
+                        break;
+                    }
+                    case this.Characteristic.RemoteKey.BACK: {
+                        this.log.info('set Remote Key Pressed: BACK');
+                        this.remoteButton("Back");
+                        break;
+                    }
+                    case this.Characteristic.RemoteKey.EXIT: {
+                        this.log.info('set Remote Key Pressed: EXIT');
+                        this.remoteButton("Back");
+                        break;
+                    }
+                    case this.Characteristic.RemoteKey.PLAY_PAUSE: {
+                        this.log.info('set Remote Key Pressed: PLAY_PAUSE');
+                        this.remoteButton("AmbilightOnOff");
+                        break;
+                    }
+                    case this.Characteristic.RemoteKey.INFORMATION: {
+                        this.log.info('set Remote Key Pressed: INFORMATION');
+                        this.remoteButton("Home");
+                        break;
+                    }
+                }
+
+                callback(null);
+            });
+
+        /**
+         * Create a speaker service to allow volume control
+         */
+
+        const speakerService = this.tvAccessory.addService(this.Service.TelevisionSpeaker);
+
+        speakerService
+            .setCharacteristic(this.Characteristic.Active, this.Characteristic.Active.ACTIVE)
+            .setCharacteristic(this.Characteristic.VolumeControlType, this.Characteristic.VolumeControlType.ABSOLUTE);
+
+        // Handle volume changes
+        speakerService.getCharacteristic(this.Characteristic.VolumeSelector)
+            .on('set', (newValue, callback) => {
+                this.log.info('set VolumeSelector => setNewValue: ' + newValue);
+
+                if (newValue == 0) {
+                    // Volume up
+                    that.log("Sending VolumeUp key");
+                    this.remoteButton("VolumeUp");
+                } else if (newValue == 1) {
+                    // Volume down
+                    that.log("Sending VolumeDown key");
+                    this.remoteButton("VolumeDown");
+                } else {
+                    // Unknown?
+                    that.log("ERROR: UNKNOWN VOLUMESELECTOR VALUE (" + newValue + ")");
+                }
+
+                callback(null);
+            });
+
+        speakerService.getCharacteristic(this.Characteristic.Mute)
+            .on('set', (newValue, callback) => {
+                this.log("Speakers new value: " + newValue);
+                var muted = newValue == 1;
+                var body = {
+                    "muted": muted
+                };
+
+                this.post("audio/volume", body);
+
+                callback(null);
+            })
+            .on('get', (callback) => {
+                var mute = 0;
+                callback(null, mute);
+            });
+
+        /**
+         * Create TV Input Source Services to use as ambilight styles
+         * These are the inputs the user can select from.
+         * When a user selected an input the corresponding Identifier Characteristic
+         * is sent to the TV Service ActiveIdentifier Characteristic handler.
+         */
+        
+        var identifier = 0;
+        ambilightStyles.forEach((item) => {
+            const ambilightStyleService = this.tvAccessory.addService(this.Service.InputSource, item.value, item.name);
+            ambilightStyleService
+                .setCharacteristic(this.Characteristic.Identifier, ++identifier)
+                .setCharacteristic(this.Characteristic.ConfiguredName, item.name)
+                .setCharacteristic(this.Characteristic.IsConfigured, this.Characteristic.IsConfigured.CONFIGURED)
+                .setCharacteristic(this.Characteristic.InputSourceType, this.Characteristic.InputSourceType.HDMI);
+
+            tvService.addLinkedService(ambilightStyleService);
+        });
+
+        // Publish as external accessory as only one TV can be registered per bridge.
+        
+        this.api.publishExternalAccessories(PLUGIN_NAME, [this.tvAccessory]);
+
+        this.remoteButton = function (button) {
+            var body = {
+                "key": button
+            };
+            this.post("input/key", body);
+        }
+
+        this.setAmbilight = function (style) {
+
+            var body = {
+                "styleName": style.type,
+                "isExpert": false,
+            };
+            if ('str' in style) {
+                body['stringValue'] = style.str;
+            } else {
+                body['menuSetting'] = style.value;
+            }
+            this.post("ambilight/currentconfiguration", body);
+        }
+
+        this.post = function (endpoint, body) {
+            const url = baseURL + endpoint;
+
+            var options = {
+                body: JSON.stringify(body),
+                rejectUnauthorized: false,
+                timeout: 2000,
+                forever: true,
+                followAllRedirects: true
+            };
+
+            request.post(url, options, function (err, response, body) {
+                if (!err && response.statusCode == 200) {
+                    // Success
+                    return body;
+                } else if (response) {
+                    code = response.statusCode || "?";
+                    that.log("Error (" + code + ") during POST request to " + url);
+                    return false;
+                }
+            })
+        }
+
+        this.get = function (endpoint, callback) {
+            const url = baseURL + endpoint;
+
+            var options = {
+                rejectUnauthorized: false,
+                timeout: 750,
+                forever: true,
+                followAllRedirects: true
+            };
+
+            request.get(url, options, (err, response, body) => {
+                if (!err && response.statusCode == 200) {
+                    // Success
+                    callback(body);
+                } else {
+                    that.log("Error during GET request (code %s)", response)
+                    callback(false);
+                }
+            })
+        }
+    }
+}
